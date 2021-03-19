58cb674c2241a6d8c90f83ed9d1ead223b4a8965
aryaman <jainaryaman123@gmail.com>
2021-03-19 17:21:47 +0530

    completed log_message_graph1

--- solve.py
+++ solve.py
@@ -1,24 +1,32 @@
 #Check function is functions having syslog or printf in its name (logging Procedures)
 #Now finding lms string using backward traversal
 #Also do Peephole concretization
+#improve get_predecessor_string now in O((n+m)^2) have to do in O((n+m))
 import angr
 import claripy
+import pickle
 import pyvex
 import string
 import itertools
+import networkx as nx
+import matplotlib.pyplot as plt
+from angrutils import *
 
-proj = angr.Project("apache2",load_options={'auto_load_libs':False})
+proj = angr.Project("skod",load_options={'auto_load_libs':False})
 
 def log_functions():
   check_func = set()
-  cfg  = proj.analyses.CFGFast(resolve_indirect_jumps = True,data_references=True,cross_references=True)
+  not_check_func = set()
+  cfg  = proj.analyses.CFGFast(normalize = True,resolve_indirect_jumps = True,data_references=True,cross_references=True)
   entry_func = cfg.kb.functions.items()
   for addr,func in entry_func:
     if 'print' in func.name:
       check_func.add(addr)
     if 'syslog' in func.name:
     	check_func.add(addr)
-  return cfg,check_func
+    else:
+      not_check_func.add(addr)
+  return cfg,check_func,not_check_func
 
 def extract_call_sites(cfg,check_func):
 	req_func = set()
@@ -36,14 +44,15 @@ def symbolic_execution(addr,cfg):
   try:
     entry_func = cfg.kb.functions[addr]
   except :
-    print("do something about it")
+    print("")
   else:
     try:
       temp_string = entry_func.string_references(minimum_length=4,vex_only=True)
     except:
       print("")
     else:
-      req_strings = temp_string
+      for addr,string in temp_string:
+        req_strings.append(string)
       return req_strings
     return []
   return []
@@ -72,21 +81,21 @@ def peephole(cfg,call_sites,max_back_trace):
             set_call.add((temp_predec,back_trace+1))
   return req_string
 
-# def hexescape(s):
-#   '''
-#   perform hex escaping on a raw string s
-#   '''
-#   out = []
-#   acceptable = (string.ascii_letters + string.digits + " .%?").encode()
-#   for c in s:
-#       if c not in acceptable:
-#           continue
-#       else:
-#           out.append(chr(c))
+def hexescape(s):
+  '''
+  perform hex escaping on a raw string s
+  '''
+  out = []
+  acceptable = (string.ascii_letters + string.digits + " .%?").encode()
+  for c in s:
+      if c not in acceptable:
+          continue
+      else:
+          out.append(chr(c))
 
-#   fnd_string = ''.join(out)
-#   if(len(fnd_string)>2):
-#     return fnd_string
+  fnd_string = ''.join(out)
+  if(len(fnd_string)>2):
+    return fnd_string
 
 
 # def random(cfg):
@@ -111,11 +120,187 @@ def peephole(cfg,call_sites,max_back_trace):
 #       req_strings.add(s_val)
 #   return req_strings
 
+# def build_lms_paths(cfg,lms_strings,cfg_functions):
+#   paths = set()
+#   for function in cfg_functions:
+#     temp = get_local_paths(cfg,function,lms_strings)
+
+def get_local_paths(cfg,function,lms_strings):
+  entry_func = cfg.kb.functions[function]
+  local_grp = entry_func._local_transition_graph
+  print(local_grp)
+
+st = proj.factory.entry_state()
+# def testing_purposes(addr,cfg):
+#   entry_func = cfg.kb.functions[addr]
+#   bbs = entry_func.blocks
+#   for bb_addr in bbs:
+#     try:
+#       ins_addr = bb_addr.instruction_addrs
+#     except:
+#       continue
+#     else:
+#       starting = min(ins_addr)
+#       ending = max(ins_addr)
+#       references = cfg.kb.xrefs
+#       temp = references.get_xrefs_by_ins_addr_region(starting, ending)
+#       tmp = list(temp)
+#       for temptmp in tmp:
+#         try:
+#           t = temptmp.memory_data
+#         except TypeError:
+#           continue
+#         else:
+#           if t!=None: 
+#             if t.sort!=None and 'string' in t.sort and t.size>1:
+#               req_addr = t.address
+#               yeah = st.memory.load(req_addr,t.size-1)
+#               print(st.solver.eval(yeah,cast_to=bytes))
 
 
-cfg,check_func = log_functions()
+def testing_purposes2(node,cfg):
+  try:
+    ins_addr = node.instruction_addrs
+  except:
+    return ''
+  else:
+    if(len(ins_addr)>0):
+      starting = min(ins_addr)
+      ending = max(ins_addr)
+      references = cfg.kb.xrefs
+      temp = references.get_xrefs_by_ins_addr_region(starting, ending)
+      tmp = list(temp)
+      for temptmp in tmp:
+        try:
+          t = temptmp.memory_data
+        except TypeError:
+          continue
+        else:
+          if t!=None: 
+            if t.sort!=None and 'string' in t.sort and t.size>1:
+              req_addr = t.address
+              yeah = st.memory.load(req_addr,t.size-1)
+              temp_string = st.solver.eval(yeah,cast_to=bytes).decode("utf-8")
+              if temp_string in req_strings:
+                return temp_string
+
+  return ''
+
+visited = set()
+queue = []
+req_graph = {}
+
+def get_predecessor_string(cfg,node,max_for_trace):
+  cur_predecessor = node.predecessors
+  predecessor = []
+  for ele in cur_predecessor:
+    predecessor.append([ele,1])
+  strings = set()
+  while(len(predecessor)!=0):
+    predec,trace = predecessor.pop()
+    have_string = testing_purposes2(predec,cfg)
+    if len(have_string)>0 :
+      strings.add(have_string)
+      if(len(predecessor)==0):
+        break
+    
+    elif trace<max_for_trace:
+      pred_pred = predec.predecessors
+      for ele in pred_pred:
+        predecessor.append([ele,trace+1])
+  return strings
+
+# def get_predecessor_string2(cfg,node,max_for_trace):
+#   node_vis = []
+#   cur_predecessor = node.predecessors
+#   predecessor = []
+#   for ele in cur_predecessor:
+#     if ele not in node_vis:
+#       predecessor.append(ele)
+#       node_vis.append(ele)
+#     strings = set()
+#     while(len(predecessor)!=0):
+#       predec = predecessor.pop()
+#       have_string = testing_purposes2(predec,cfg)
+#       if len(have_string)>0 and have_string in req_strings:
+#         strings.add(have_string)
+#         if(len(predecessor)==0):
+#           break
+      
+#       else :
+#         pred_pred = predec.predecessors
+#         for ele in pred_pred:
+#           predecessor.append(ele)
+#     return strings
+
+
+def do_bfs(cfg,node,max_for_trace=7):
+  visited.add(node)
+  queue.append(node)
+  while queue:
+    s = queue.pop(0)
+    # have_string = testing_purposes2(s,cfg)
+    # flag=0
+    # if have_string not in req_graph and len(have_string)>1:
+    #   req_graph[have_string] = set()
+    #   flag = 1
+    neighbours = s.successors
+    if neighbours!=None and len(neighbours)>0:
+      for neighbour in neighbours:
+        if neighbour not in visited:
+          visited.add(neighbour)
+          queue.append(neighbour)
+          child_have_string = testing_purposes2(neighbour,cfg)
+          if len(child_have_string)>1:
+            # req_graph[have_string].add(child_have_string)
+            predecessor_strings = get_predecessor_string(cfg,neighbour,max_for_trace)
+            for strin in predecessor_strings:
+              if strin not in req_graph:
+                req_graph[strin] = set()
+              req_graph[strin].add(child_have_string)
+
+
+# def do_bfs2(cfg,node,max_for_trace=5):
+#   child_have_string = testing_purposes2(node,cfg)
+#   if len(child_have_string)>1:
+#     predecessor_strings = get_predecessor_string2(cfg,node,max_for_trace)
+#     if predecessor_strings==None:
+#       return
+#     for strin in predecessor_strings:
+#       if strin not in req_graph:
+#         req_graph[strin] = set()
+#       req_graph[strin].add(child_have_string)
+
+class GraphVisualization: 
+   
+    def __init__(self): 
+          
+        self.visual = [] 
+          
+    def addEdge(self, a, b): 
+        temp = [a, b] 
+        self.visual.append(temp) 
+          
+    def visualize(self): 
+        G = nx.Graph() 
+        G.add_edges_from(self.visual) 
+        nx.draw_networkx(G) 
+        plt.show() 
+
+
+
+cfg,check_func,not_check_func = log_functions()
 req_func = extract_call_sites(cfg,check_func)
 req_strings = peephole(cfg,req_func,3)
-print(req_strings)
+temp = cfg.graph.nodes()
+for node in temp:
+  if node not in visited:
+    do_bfs(cfg,node)
+print(req_graph)
+# G = GraphVisualization()
+# for key in req_graph.keys():
+#   neighbours = req_graph[key]
+#   for neighbour in neighbours:
+#     G.addEdge(key,neighbour)
 
-
+# G.visualize()
